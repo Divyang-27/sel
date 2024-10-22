b0a0d3ee663ce459003246bca13d9b7f6016b137
Divyang Chhelavda <divyang.chhelavda.02@gmail.com>
2024-10-22 16:01:28 +0530

    Update Heoku_Login.java

--- src/test/java/login/Heoku_Login.java
+++ src/test/java/login/Heoku_Login.java
@@ -1,26 +1,47 @@
-package login;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Heoku_Login  {
+public class BruteForceLoginTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 // Set the path to the ChromeDriver executable
+    public static String van = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+    WebDriver driver;
 
-        // Initialize Chrome browser
-        WebDriver driver = new ChromeDriver();
+    @BeforeTest
+    public void bt() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get(van);
+        driver.manage().window().maximize();
+    }
 
-        // Navigate to the website
-        driver.get("https://rahulshettyacademy.com");
+    @Test(dataProvider = "credentialsProvider")
+    public void tc(String username, String password) throws InterruptedException {
+        driver.findElement(By.name("username")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        
+        Thread.sleep(2000);  // Pause to see the input before submission
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
+        
+        // Wait and refresh after submission to test the next set of credentials
+        Thread.sleep(3000); 
+        driver.navigate().refresh();
+    }
 
-        // Print the title and current URL of the page
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
-
-        // Close the browser
-        driver.close();
-	}
-
+    @DataProvider
+    public Object[][] credentialsProvider() {
+        return new Object[][] {
+            // Add more credentials to attempt brute force login
+            new Object[] { "Admin", "admin123" },
+            new Object[] { "User1", "wrongPassword" },
+            new Object[] { "User2", "pass123" },
+            new Object[] { "Admin", "wrongPass" }
+            // Continue adding as many combinations as necessary
+        };
+    }
 }
+
